acdc61d1c21fc0f4a88d5492b29d77ee0b0bc302
Regal <regal@cobra.monster>
2025-06-10 23:40:09 -0500

    Added compact mode

--- src/utils/stringFormatters.ts
+++ src/utils/stringFormatters.ts
@@ -1,3 +1,8 @@
 export const formatDate = (date: Date) => {
   return date?.toLocaleString("en-US", { day: "2-digit", month: "long", year: "numeric", timeZone: "UTC" });
 };
+
+export const capitalizeFirst = (input: string) => {
+  if (!input) return input;
+  return input[0].toUpperCase() + input.slice(1);
+};
