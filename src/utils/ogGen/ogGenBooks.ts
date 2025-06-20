e68795af59998084ef8875d46bd3ef0a51c4f91b
Regal <regal@cobra.monster>
2025-06-20 14:40:22 -0400

    hard-coded path

--- src/utils/ogGen/ogGenBooks.ts
+++ src/utils/ogGen/ogGenBooks.ts
@@ -64,7 +64,7 @@ export const booksHtml = ({ title, subtitle, date, rating, cover }: OgGenBlogPro
         </div>
         <!-- Image -->
         <div class="flex right-10 bottom-10 mb-[80px] justify-end items-end">
-          <img src="${baseUrl}books/${cover}" alt="${title} Book Cover" height="550px" width="400px"/>
+          <img src="${baseUrl}books/all-systems-red.jpg" height="550px" width="400px"/>
         </div>
       </div>
       <!-- Footer -->
