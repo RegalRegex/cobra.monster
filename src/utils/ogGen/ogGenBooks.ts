f807e160f3046f1f6ac45d586181b0c00951e212
Regal <regal@cobra.monster>
2025-06-20 14:13:16 -0400

    moved to public

--- src/utils/ogGen/ogGenBooks.ts
+++ src/utils/ogGen/ogGenBooks.ts
@@ -64,7 +64,7 @@ export const booksHtml = ({ title, subtitle, date, rating, cover }: OgGenBlogPro
         </div>
         <!-- Image -->
         <div class="flex right-10 bottom-10 mb-[80px] justify-end items-end">
-          <img src="${baseUrl}books/all-systems-red.png"/>
+          <img src="${baseUrl}all-systems-red.jpg"/>
         </div>
       </div>
       <!-- Footer -->
