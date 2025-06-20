48213f8752853bf671495b4349d0ee7f2174b309
Regal <regal@cobra.monster>
2025-06-20 14:07:08 -0400

    Testing with commie cross

--- src/utils/ogGen/ogGenBooks.ts
+++ src/utils/ogGen/ogGenBooks.ts
@@ -64,7 +64,7 @@ export const booksHtml = ({ title, subtitle, date, rating, cover }: OgGenBlogPro
         </div>
         <!-- Image -->
         <div class="flex right-10 bottom-10 mb-[80px] justify-end items-end">
-          <img src="${baseUrl}books/all-systems-red.jpg" class="h-[550px]"/>
+          <img src="${baseUrl}CommieCross-transparent_Batnoise.png" class="h-[550px]"/>
         </div>
       </div>
       <!-- Footer -->
