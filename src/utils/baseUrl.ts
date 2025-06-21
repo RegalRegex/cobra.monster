8f4d3d9bf202010f8e4d100c63d751f4afb50022
Regal <regal@cobra.monster>
2025-06-20 20:50:46 -0400

    set local env vars

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,7 +1,9 @@
 export const baseUrl = () => {
-  if (import.meta.env.PUBLIC_IS_TEST_BRANCH === true) {
+  const test = import.meta.env.PUBLIC_IS_TEST_BRANCH;
+  const prod = import.meta.env.PROD;
+  if (test) {
     return "https://test-branch.cobra.monster/";
-  } else if (import.meta.env.PROD === true) {
+  } else if (prod) {
     return "https://cobra.monster/";
   } else {
     return "http://localhost:4321/";
