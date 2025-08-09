5415d9b41a12e9c58e1e3b51273c558e2ed8fc72
Regal <regal@cobra.monster>
2025-08-09 14:58:12 -0500

    Finalized first draft of Gallery stuff

--- src/temp/_galleries.ts
+++ src/temp/_galleries.ts
@@ -1,3 +1,13 @@
+/**
+ * Image-fetching logic using Astro's glob loader.
+ * Currently UNUSED but keeping it in case I need it later.
+ *
+ * Didn't use it because it only loads images without the ability to load them with captions, alt text, etc.
+ *
+ * Circumvented this in Collections and @see GalleryGrid
+ * Referenced from: {@link https://jankraus.net/2024/04/05/how-to-build-a-simple-photo-gallery-with-astro/}
+ */
+
 export async function getGalleryImages(galleryId: string) {
   // 1. List all album files from collections path
   let images = import.meta.glob<{ default: ImageMetadata }>("/src/assets/galleries/**/*.{jpeg,jpg,png}");
