ff15de4d9f92a9b4a78dc523f56aea21c90304db
Regal <regal@cobra.monster>
2026-03-31 09:41:27 -0500

    Remove timestamp altogether. lol.

--- src/utils/stringFormatters.ts
+++ src/utils/stringFormatters.ts
@@ -6,10 +6,6 @@ export const formatLocalDate = (date: Date) => {
   return date?.toLocaleString("en-US", { day: "2-digit", month: "long", year: "numeric" });
 };
 
-export const formatTime = (date: Date) => {
-  return date?.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
-};
-
 export const capitalizeFirst = (input: string) => {
   if (!input) return input;
   return input[0].toUpperCase() + input.toLowerCase().slice(1);
