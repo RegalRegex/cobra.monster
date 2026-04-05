fa22ea4f36046938d48c39b3177c6991427e917c
Regal <regal@cobra.monster>
2026-04-05 12:12:29 -0500

    Finished blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -26,7 +26,7 @@ const posts = defineCollection({
       tags: z.array(reference("tags")).optional(),
       headerImg: image().optional(),
       headerImgCaption: z.string().optional(),
-      headerOg: z.boolean().optional(),
+      headerOgImg: image().optional(),
     }),
 });
 
