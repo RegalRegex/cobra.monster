54ddc5349c9756040894e5f6f5c924155f837c93
Regal <regal@cobra.monster>
2026-04-05 12:18:22 -0500

    Revert og img change

--- src/content/config.ts
+++ src/content/config.ts
@@ -26,7 +26,7 @@ const posts = defineCollection({
       tags: z.array(reference("tags")).optional(),
       headerImg: image().optional(),
       headerImgCaption: z.string().optional(),
-      headerOgImg: image().optional(),
+      headerOg: z.boolean().optional(),
     }),
 });
 
